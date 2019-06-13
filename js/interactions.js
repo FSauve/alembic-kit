6bbbb964b2ff1c23d2ea983c045a27db36edb599
Frederic LS <>
2019-06-13 08:51:36 -0400

    Integration de l'outil externe smtpJS afin d'envoyer des courriels directement à notre boite courriel

--- js/interactions.js
+++ js/interactions.js
@@ -6,3 +6,17 @@ function hideDiv() {
         x.style.display = "none";
     }
 }
+
+function envoyerEmail2Trimoz(){
+    var selectedIndex = document.getElementById("agee").selectedIndex;
+
+    Email.send({
+        SecureToken :"1b45d8f3-9bff-4a3a-abaf-48901692c70c",
+        To : 'fsauve@trimoz.com',
+        From : "fsauve@trimoz.com",
+        Subject : "This is the subject",
+        Body : "Index agée selectionné = "+selectedIndex
+    }).then(
+        message => alert(message)
+);
+}
